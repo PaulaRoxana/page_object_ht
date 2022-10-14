c9c393a0ffd0affa7224defe65927afef9e25b5e
Tamas Demeny <Tamas_Demeny@epam.com>
2022-10-14 10:29:49 +0300

    Add and Remove Items

--- src/main/java/org/example/amazon_page_object/ProductPage.java
+++ src/main/java/org/example/amazon_page_object/ProductPage.java
@@ -1,10 +1,24 @@
 package org.example.amazon_page_object;
 
+import org.example.modules.ConfirmationMessage;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.time.Duration;
 
 public class ProductPage extends BasePage{
-
+    public static final By ADD_TO_CART = By.xpath("//input[@id=\"add-to-cart-button\"]");
     public ProductPage(WebDriver driver) {
         super(driver);
+    }
+
+    public ConfirmationMessage addToCart(){
+        new WebDriverWait(driver, Duration.ofSeconds(20))
+                .until(ExpectedConditions.visibilityOfElementLocated(ADD_TO_CART))
+                .click();
+        return new ConfirmationMessage(driver);
     }
 }
---
+++ src/test/java/org/example/amazon_tests/AddOrRemoveItems.java
@@ -0,0 +1,43 @@
+package org.example.amazon_tests;
+
+import org.example.amazon_page_object.CartPage;
+import org.example.amazon_page_object.HomePage;
+import org.example.amazon_page_object.TopBar;
+import org.example.modules.ConfirmationMessage;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class AddOrRemoveItems extends BaseTests{
+    @Test
+    public void addToCartFunctionality() {
+        TopBar homePage = new TopBar(driver);
+        ConfirmationMessage addedToCartPage = homePage.searchMethod("keyboards")
+                .clickRedragon()
+                .addToCart();
+
+        String addedToCartMessage = addedToCartPage.addedToCartMessage();
+        Assert.assertEquals(addedToCartMessage, "Added to Cart",
+                "The message \"Added to Cart\" did not appear!");
+
+        String amountOfProductInCart = addedToCartPage.amountOfProductInCart();
+        Assert.assertEquals(amountOfProductInCart, "1",
+                "The amount of products in cart should be 1!");
+    }
+
+    @Test
+    public void removeFromCart() {
+        TopBar homePage = new TopBar(driver);
+        CartPage cartEmptyPage = homePage
+                .searchMethod("keyboards")
+                .clickRedragon()
+                .addToCart()
+                .openCart()
+                .deleteFromCartButton();
+
+        Assert.assertEquals(cartEmptyPage.cartEmptyMessage(), "Your Amazon Cart is empty.",
+                "The message \"Your Amazon Cart is empty.\" did not appear!");
+
+        Assert.assertEquals(cartEmptyPage.cartSum(), "$0.00",
+                "The payable amount did not go down to $0!");
+    }
+}
